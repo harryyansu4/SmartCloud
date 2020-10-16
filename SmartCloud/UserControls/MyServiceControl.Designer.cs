562f5de38a539d30fd75ab25e1652347e4285efb
harryyansu4 <446535924@qq.com>
2020-10-16 16:55:20 +0800

    commit-2020.10.16 16:55

--- SmartCloud/UserControls/MyServiceControl.Designer.cs
+++ SmartCloud/UserControls/MyServiceControl.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyServiceControl));
             this.LeftPanel = new System.Windows.Forms.Panel();
             this.FileListView = new System.Windows.Forms.ListView();
@@ -36,6 +37,8 @@
             this.BrowseButton = new System.Windows.Forms.Button();
             this.ExecuteButton = new System.Windows.Forms.Button();
             this.CenterPanel = new System.Windows.Forms.Panel();
+            this.FileListImageList = new System.Windows.Forms.ImageList(this.components);
+            this.GoBackButton = new System.Windows.Forms.Button();
             this.LeftPanel.SuspendLayout();
             this.LeftPanel_Bar.SuspendLayout();
             this.SuspendLayout();
@@ -56,16 +59,21 @@
             this.FileListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.FileListView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.FileListView.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FileListView.FullRowSelect = true;
+            this.FileListView.GridLines = true;
             this.FileListView.HideSelection = false;
             this.FileListView.Location = new System.Drawing.Point(0, 30);
             this.FileListView.Name = "FileListView";
             this.FileListView.Size = new System.Drawing.Size(420, 480);
             this.FileListView.TabIndex = 1;
             this.FileListView.UseCompatibleStateImageBehavior = false;
+            this.FileListView.View = System.Windows.Forms.View.Details;
+            this.FileListView.DoubleClick += new System.EventHandler(this.FileListView_DoubleClick);
             // 
             // LeftPanel_Bar
             // 
             this.LeftPanel_Bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(240)))), ((int)(((byte)(222)))));
+            this.LeftPanel_Bar.Controls.Add(this.GoBackButton);
             this.LeftPanel_Bar.Controls.Add(this.FilePathTextBox);
             this.LeftPanel_Bar.Controls.Add(this.BrowseButton);
             this.LeftPanel_Bar.Controls.Add(this.ExecuteButton);
@@ -81,9 +89,9 @@
             this.FilePathTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.FilePathTextBox.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FilePathTextBox.ForeColor = System.Drawing.Color.Black;
-            this.FilePathTextBox.Location = new System.Drawing.Point(4, 6);
+            this.FilePathTextBox.Location = new System.Drawing.Point(40, 6);
             this.FilePathTextBox.Name = "FilePathTextBox";
-            this.FilePathTextBox.Size = new System.Drawing.Size(322, 18);
+            this.FilePathTextBox.Size = new System.Drawing.Size(305, 18);
             this.FilePathTextBox.TabIndex = 1;
             // 
             // BrowseButton
@@ -95,9 +103,9 @@
             this.BrowseButton.FlatAppearance.BorderSize = 0;
             this.BrowseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BrowseButton.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.BrowseButton.Location = new System.Drawing.Point(332, 2);
+            this.BrowseButton.Location = new System.Drawing.Point(350, 2);
             this.BrowseButton.Name = "BrowseButton";
-            this.BrowseButton.Size = new System.Drawing.Size(40, 26);
+            this.BrowseButton.Size = new System.Drawing.Size(32, 26);
             this.BrowseButton.TabIndex = 2;
             this.BrowseButton.UseVisualStyleBackColor = false;
             this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
@@ -111,9 +119,9 @@
             this.ExecuteButton.FlatAppearance.BorderSize = 0;
             this.ExecuteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExecuteButton.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ExecuteButton.Location = new System.Drawing.Point(376, 2);
+            this.ExecuteButton.Location = new System.Drawing.Point(385, 2);
             this.ExecuteButton.Name = "ExecuteButton";
-            this.ExecuteButton.Size = new System.Drawing.Size(40, 26);
+            this.ExecuteButton.Size = new System.Drawing.Size(32, 26);
             this.ExecuteButton.TabIndex = 0;
             this.ExecuteButton.UseVisualStyleBackColor = false;
             this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
@@ -126,6 +134,29 @@
             this.CenterPanel.Name = "CenterPanel";
             this.CenterPanel.Size = new System.Drawing.Size(340, 510);
             this.CenterPanel.TabIndex = 1;
+            // 
+            // FileListImageList
+            // 
+            this.FileListImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FileListImageList.ImageStream")));
+            this.FileListImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.FileListImageList.Images.SetKeyName(0, "文件夹.png");
+            this.FileListImageList.Images.SetKeyName(1, "文件.png");
+            // 
+            // GoBackButton
+            // 
+            this.GoBackButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GoBackButton.BackColor = System.Drawing.Color.OliveDrab;
+            this.GoBackButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("GoBackButton.BackgroundImage")));
+            this.GoBackButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.GoBackButton.FlatAppearance.BorderSize = 0;
+            this.GoBackButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoBackButton.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.GoBackButton.Location = new System.Drawing.Point(3, 2);
+            this.GoBackButton.Name = "GoBackButton";
+            this.GoBackButton.Size = new System.Drawing.Size(32, 26);
+            this.GoBackButton.TabIndex = 3;
+            this.GoBackButton.UseVisualStyleBackColor = false;
+            this.GoBackButton.Click += new System.EventHandler(this.GoBackButton_Click);
             // 
             // MyServiceControl
             // 
@@ -153,5 +184,7 @@
         private System.Windows.Forms.Button ExecuteButton;
         private System.Windows.Forms.TextBox FilePathTextBox;
         private System.Windows.Forms.ListView FileListView;
+        private System.Windows.Forms.ImageList FileListImageList;
+        private System.Windows.Forms.Button GoBackButton;
     }
 }
--- SmartCloud/UserControls/MyServiceControl.cs
+++ SmartCloud/UserControls/MyServiceControl.cs
@@ -16,7 +16,7 @@ namespace SmartCloud.UserControls
     public partial class MyServiceControl : UserControl
     {
         /// <summary>
-        /// 
+        /// 读取到的文件全路径集合
         /// </summary>
         private List<string> FilePathList = new List<string>();
         /// <summary>
@@ -24,9 +24,13 @@ namespace SmartCloud.UserControls
         /// </summary>
         /// <param name="value"></param>
         private delegate void ShowInfoDelegate(ListViewItem listViewItem);
+        /// <summary>
+        /// 文件根路径（用于校验当前路径是否越过根路径）
+        /// </summary>
+        private string RootFilePath = "";
 
         /// <summary>
-        /// 总文件路径
+        /// 文件路径
         /// </summary>
         public string FilePath = "";
         /// <summary>
@@ -37,12 +41,22 @@ namespace SmartCloud.UserControls
         public MyServiceControl()
         {
             InitializeComponent();
+            // 初始化ListView的列表头
+            ColumnHeader chForListView = new ColumnHeader();
+            chForListView.Width = 280;
+            chForListView.Text = "文件名";
+            chForListView.TextAlign = HorizontalAlignment.Left;// 设置列的对齐方式
+            this.FileListView.Columns.Add(chForListView);
+            // 将listView的图标集与imageList1绑定
+            this.FileListView.SmallImageList = this.FileListImageList;
             // 实例化线程队列
             ProcessInfoQueue = new ConcurrentQueue<string>();
         }
 
+        #region 开始执行时FileListView信息回显的线程执行区域
         public void ShowInfoOnTreeView()
         {
+            this.FilePathList.Clear();
             bool processControl = true;// 循环控制
             while (processControl)
             {
@@ -55,35 +69,72 @@ namespace SmartCloud.UserControls
                 {
                     // 是否未结束
                     if (!"process is finished".Equals(infoStr))
-                    {
                         FilePathList.Add(infoStr);
-                    }
                     else
-                    {
                         processControl = false;
-                    }
                 }
             }
-
             if (this.FilePathList.Count <= 0)
                 return;
-
-            for (int i = 0; i <= FilePathList.Count - 1; i++)
+            // 文件路径转换
+            List<ListViewItem> itemList = TransforFilePath(this.FilePath, this.FilePathList);
+            // 界面回显
+            for (int i = 0; i <= itemList.Count - 1; i++)
             {
-                ListViewItem listViewItem = new ListViewItem();
-                listViewItem.Text = FilePathList[i];
-
                 if (this.FileListView.InvokeRequired)
-                    this.FileListView.BeginInvoke(new ShowInfoDelegate(ShowInfo), new object[] { listViewItem });
+                    this.FileListView.BeginInvoke(new ShowInfoDelegate(ShowInfo), new object[] { itemList[i] });
                 else
-                    this.FileListView.Items.Add(listViewItem);
+                    this.FileListView.Items.Add(itemList[i]);
             }
-            MessageBox.Show("结束");
         }
 
         private void ShowInfo(ListViewItem lvItem)
         {
             this.FileListView.Items.Add(lvItem);
+        }
+        #endregion
+
+        /// <summary>
+        /// 从集合中获取到当前路径下的文件和文件夹，并形成列信息
+        /// </summary>
+        /// <param name="filePath">上一级全路径名</param>
+        /// <param name="filePathList">待转换的全路径名集合</param>
+        /// <returns></returns>
+        private List<ListViewItem> TransforFilePath(string filePath, List<string> filePathList)
+        {
+            List<ListViewItem> itemList = new List<ListViewItem>();
+            // 用于记录可回显的文件和文件夹名称
+            List<string> checkList = new List<string>();
+            for (int i = 0; i <= filePathList.Count - 1; i++)
+            {
+                // 获取当前目录下的文件和文件夹
+                string newFilePath = filePathList[i].Replace(filePath, "");
+                // 集合中的路径可能和上一级路径相同，导致替换完变成空字符创
+                if (newFilePath == null || "".Equals(newFilePath))
+                    continue;
+                string[] strs = newFilePath.Substring(1, newFilePath.Length - 1).Split('\\');
+                // 排除重复的回显名称
+                if (checkList.Contains(strs[0]))
+                    continue;
+                checkList.Add(strs[0]);
+                // 创建列
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = strs[0];// 显示名称
+                listViewItem.Tag = filePath + "\\" + strs[0];// 记录当前文件或文件夹的全路径名
+                // 图片标识、名称
+                if (strs.Length <= 1 && strs[0].Contains("."))
+                {
+                    listViewItem.Name = "file";
+                    listViewItem.ImageIndex = 1;
+                }
+                else
+                {
+                    listViewItem.Name = "directory";
+                    listViewItem.ImageIndex = 0;
+                }
+                itemList.Add(listViewItem);
+            }
+            return itemList;
         }
 
         /// <summary>
@@ -110,6 +161,7 @@ namespace SmartCloud.UserControls
                 if (!string.IsNullOrEmpty(FilePath))
                 {
                     this.FilePathTextBox.Text = this.FilePath;
+                    this.RootFilePath = this.FilePath;
                 }
             }
         }
@@ -126,6 +178,16 @@ namespace SmartCloud.UserControls
                 MessageBox.Show("未选择文件路径信息...");
                 return;
             }
+            if (!string.IsNullOrEmpty(RootFilePath) && !FilePath.Equals(RootFilePath))
+            {
+                MessageBoxButtons messageBtn = MessageBoxButtons.OKCancel;
+                DialogResult dr = MessageBox.Show("再次执行将更改初始目录，是否继续...", "目录更改提示", messageBtn);
+                if (dr == DialogResult.OK)
+                    this.RootFilePath = this.FilePath;
+                else
+                    return;
+            }
+            this.FileListView.Items.Clear();
             // 开启TreeView信息回显执行线程
             Thread t = new Thread(new ThreadStart(ShowInfoOnTreeView));
             t.Start();
@@ -133,6 +195,82 @@ namespace SmartCloud.UserControls
             MyServiceTask msTask = new MyServiceTask();
             msTask.Start(this.FilePath, this.ProcessInfoQueue);
 
+        }
+
+        /// <summary>
+        /// FileListView信息双击事件（双击进入文件夹）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileListView_DoubleClick(object sender, EventArgs e)
+        {
+            // 获取当前选中的行信息
+            ListViewItem focusItem = this.FileListView.FocusedItem;
+            if (focusItem == null)
+                return;
+            if ("file".Equals(focusItem.Name))
+                return;
+            // 获取当前全路径名
+            string filePath = focusItem.Tag.ToString();
+            // 回显路径信息，并更新文件路径
+            this.FilePathTextBox.Text = filePath;
+            this.FilePath = filePath;
+            // 从总文件路径集合中筛选出当前文件夹相关的路径信息，并储存
+            List<string> childFileList = new List<string>();
+            for (int i = 0; i <= FilePathList.Count - 1; i++)
+            {
+                if (FilePathList[i].Contains(filePath))
+                    childFileList.Add(FilePathList[i]);
+            }
+            this.FileListView.Items.Clear();
+            // 文件路径转换，并回显
+            this.FileListView.BeginUpdate();
+            List<ListViewItem> itemList = TransforFilePath(filePath, childFileList);
+            foreach (ListViewItem item in itemList)
+            {
+                this.FileListView.Items.Add(item);
+            }
+            this.FileListView.EndUpdate();
+        }
+
+        /// <summary>
+        /// 返回按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GoBackButton_Click(object sender, EventArgs e)
+        {
+            // 路径校验
+            if (FilePath.Equals(RootFilePath))
+            {
+                MessageBox.Show("已返回到初始目录，无法再返回！");
+                return;
+            }
+            // 获取上一级路径，并跟新路径信息
+            string[] strs = this.FilePath.Split('\\');
+            string newFilePath = "";
+            for (int i = 0; i < strs.Length - 1; i++)
+            {
+                newFilePath = newFilePath + strs[i] + "\\";
+            }
+            this.FilePath = newFilePath.Substring(0, newFilePath.Length - 1);
+            this.FilePathTextBox.Text = this.FilePath;
+            // 从总文件路径集合中筛选出当前文件夹相关的路径信息，并储存
+            List<string> childFileList = new List<string>();
+            for (int i = 0; i <= FilePathList.Count - 1; i++)
+            {
+                if (FilePathList[i].Contains(FilePath))
+                    childFileList.Add(FilePathList[i]);
+            }
+            this.FileListView.Items.Clear();
+            // 文件路径转换，并回显
+            this.FileListView.BeginUpdate();
+            List<ListViewItem> itemList = TransforFilePath(FilePath, childFileList);
+            foreach (ListViewItem item in itemList)
+            {
+                this.FileListView.Items.Add(item);
+            }
+            this.FileListView.EndUpdate();
         }
     }
 }
